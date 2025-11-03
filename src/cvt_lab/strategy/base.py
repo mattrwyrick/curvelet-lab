2ce300f2fa25786ecbbc62b7bd3557d55128407a
Matt Wyrick <mattwyrick@Matts-MacBook-Pro.local>
2025-11-02 22:25:33 -0600

    init base refactor for strategy and market data

--- src/cvt_lab/strategy/base.py
+++ src/cvt_lab/strategy/base.py
@@ -1,23 +1,40 @@
 
 
-
-from cvt_lab.strategy.base import BaseTrade
-
-from cvt_lab.signal import stationarity
-
-
-class MeanReversion(BaseTrade):
+class BaseTrade(object):
 
     def __init__(
             self,
             series,
             funds,
-            position    ):
-        super().__init__(
-            series=series,
-            funds=funds,
-            position=position
-        )
+            position,
+    ):
+        self.series = series
+        self.funds = funds
+        self.position = position
+
+    def deposit_funds(self, amount, allow_negative=False, raise_exception=False):
+        if allow_negative:
+            self.funds += amount
+            return True
+        elif amount < (-1.0 * self.funds):
+            if raise_exception:
+                raise ValueError(f"Insufficient Funds. Capital: {self.funds} Add: {amount}")
+            return False
+        else:
+            self.funds += amount
+            return True
+
+    def withdraw_funds(self, amount, allow_negative=False, raise_exception=False):
+        if allow_negative:
+            self.funds -= amount
+            return True
+        elif (-1.0 * amount) > self.funds:
+            if raise_exception:
+                raise ValueError(f"Insufficient Funds. Capital: {self.funds} Add: {amount}")
+            return False
+        else:
+            self.funds -= amount
+            return True
 
     def add_data(self, new_data):
         raise NotImplemented(f"Implement add_new_data(...) in this class: {type(self)}")
@@ -46,26 +63,3 @@ class MeanReversion(BaseTrade):
     def should_exit_position(self):
         raise NotImplemented(f"Implement should_exit_position(...) in this class: {type(self)}")
 
-    def eval_target_ranges(self, series=None):
-        pass
-
-    def trim_to_target_range(self, series=None):
-        pass
-
-    def trim_to_next_target_range(self, series=None):
-        pass
-
-    def is_stationary(self, series=None):
-        pass
-
-    def get_lower_band(self, series=None):
-        pass
-
-    def get_upper_band(self, series=None):
-        pass
-
-    def get_mean(self, series=None):
-        pass
-
-    def get_standard_dev(self, series=None):
-        pass
